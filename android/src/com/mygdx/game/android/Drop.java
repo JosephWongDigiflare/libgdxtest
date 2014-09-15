5d17c4d37c99255ef82561effb553dc035a56fec
jwong <josephwongcl@gmail.com>
2014-09-15 01:53:15 -0400

    Added game logic for 'drop'

--- android/src/com/mygdx/game/android/Drop.java
+++ android/src/com/mygdx/game/android/Drop.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.android;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
@@ -8,9 +9,23 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class Drop extends Game {
 
-    @Override
+    public SpriteBatch batch;
+    public BitmapFont font;
+
     public void create() {
         batch = new SpriteBatch();
-        
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
     }
+
+    public void render() {
+        super.render(); //important!
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
 }

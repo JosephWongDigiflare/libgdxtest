8a49dbe5331e7525de64b7941fa9170954ca5f17
jwong <josephwongcl@gmail.com>
2014-09-15 01:39:02 -0400

    initial

---
+++ android/src/com/mygdx/game/android/Drop.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.android;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by jwong on 15/09/14.
+ */
+public class Drop extends Game {
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        
+    }
+}
